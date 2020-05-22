97d22584e50494296db131090cf8bdfdfef27ff0
kunalkk17 <43091441+kunalkk17@users.noreply.github.com>
2020-05-22 08:57:10 +0530

    Update dao.js

--- dao.js
+++ dao.js
@@ -1,28 +1,9 @@
-var sqlite3 = require('sqlite3').verbose()
-const DBSOURCE = './db/test.db'
+var Datastore = require('nedb');
+var db = new Datastore({ filename: 'instances.db' });
 
-let db = new sqlite3.Database(DBSOURCE, (err) => {
-    if (err) {
-      // Cannot open database
-      console.error(err.message)
-      throw err
-    }else{
-        console.log('Connected to the SQLite database.')
-        db.run(`CREATE TABLE user (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name text, 
-            host text,
-            port text, 
-            password text
-            )`,
-        (err) => {
-            if (err) {
-                // Table already created
-                console.log('table already created')
-            }
-        });  
-    }
+db.loadDatabase(function(err) {
+    // Start issuing commands after callback...
+    console.log("database loaded")
 });
 
-
-module.exports = db
+module.exports = db ;
